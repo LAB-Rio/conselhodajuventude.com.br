af2faafe89b491e37a17b37a84e163f0e6e87cb4
Frederick Silva <fredericksilva@outlook.com>
2016-01-29 19:53:04 +0000

    add styles

--- app.rb
+++ app.rb
@@ -2,35 +2,27 @@ require 'sinatra'
 
 enable :static
 
-get '/' do
-  send_file 'public/index.html' 
-end
-
 get '/inscricao' do
   redirect "https://docs.google.com/forms/d/14lPJko-tdXubeCcJnaeYhrh-JOuyJLB_0FeMO7n0qrM/viewform"
-end
-
-get '/transparencia' do
-  send_file 'public/transparencia.html'
-end
-
-get '/sobre' do
-  send_file 'public/sobre.html'
 end
 
 get '/calendario' do
   send_file 'public/calendario.html'
 end
 
-get '/slim' do
-  @title = "Conselho da Juventude"
-  slim :index
+get '/' do
+  @title = "Conselho da Juventude da Cidade"
+  slim :home
 end
 
-get '/sobre-slim' do
+get '/sobre' do
   slim :sobre, :layout => true
 end
 
 get '/home' do
   slim :home, :layout => true
+end
+
+get '/transparencia' do
+    slim :transparencia, :layout => true
 end
